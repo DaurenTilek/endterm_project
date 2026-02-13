e0fe6e1ccbfc6470bf269a4b82f2aa28887a6506
Dauren <kzdaren7@gmail.com>
2026-02-14 01:07:05 +0500

    create student class.and create modifiers,constructor

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private int sid;
+    private String firstname;
+    private String lastname;
+
+    public Student(int sid, String firstname, String lastname) {
+        this.sid = sid;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
